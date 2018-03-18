1874539dd8335ebcdeed1f24cac1289a7d20de83
mariaapaulah <mariaapaulah@gmail.com>
2018-03-17 20:59:28 -0500

    Initial commit

--- Hernandez-Pinball/Assets/Scripts/BounceObject.js
+++
@@ -1,8 +0,0 @@
-#pragma strict
-
-var explosionStrength : float = 100;
-
-function OnCollisionEnter (_other : Collision)
-{
-    _other.rigidbody.AddExplosionForce(explosionStrength, this.transform.position, 5);
-}
--- Hernandez-Pinball/Assets/Scripts/girarobjeto.js
+++
@@ -1,9 +0,0 @@
-﻿#pragma strict
-
-var girarx : int = 0;
-var girary : int = 0;
-var girarz : int = 0;
-
-function Update () {
-   transform.Rotate(girarx, girary, girarz);
-}
